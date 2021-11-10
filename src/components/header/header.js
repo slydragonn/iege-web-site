3bbb2d73265f91fa14b5d12474feac6c04f71f54
AL0G0 <alogo18m@gmail.com>
2021-11-10 14:09:14 -0500

    optimizacion de imagenes con gatsby-plugin-image y implementacion con styled components en header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,10 +2,25 @@ import React from 'react'
 import Navbar from './navbar/navbar'
 import './index.css'
 import escudo from '../../assets/imagenes/escudoPrincipal.png'
+import styled from 'styled-components';
+import biblio from '../../assets/imagenes/fondo.jpg'
+
+const Fondo = styled.header`
+    display: flex;
+    width: 100%;
+    height: 660px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-image: url(${props => props.bgImg});
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center center;
+`;
 
 export default function Header() {
     return (
-        <header className='header'>
+        <Fondo bgImg={biblio}>
             <Navbar/>
             <div className='presentacion'>
             <h2 className='presentacion__titulo'>Institución Educativa</h2>
@@ -14,6 +29,6 @@ export default function Header() {
             </div>
             <h2 className='presentacion__titulo'>Gabriel Echavarría</h2>
             </div>
-        </header>
+        </Fondo>
     )
 }
--- src/components/secciones/secciones.js
+++ src/components/secciones/secciones.js
@@ -1,28 +1,25 @@
 import React from 'react'
 import './index.css'
-import biblio from '../../assets/imagenes/biblioteca.jpg'
-import crono from '../../assets/imagenes/cronograma.jpg'
-import lengua from '../../assets/imagenes/señas.jpg'
-import horario from '../../assets/imagenes/horario.jpg'
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Secciones() {
     return(
         <div className='secciones'>
             <h2 className='secciones__titulo'>Para nuestros estudiantes</h2>
             <div className='secciones__item item1'>
-                <img src={biblio} width='400px' alt='biblioteca'/>
+                <StaticImage src='../../assets/imagenes/biblioteca.jpg' className='secciones__img' alt= 'biblioteca'/>
                 <h3>Biblioteca</h3>
             </div>
             <div className='secciones__item item2'>
-                <img src={crono} width='300px' alt='biblioteca'/>
+                <StaticImage src='../../assets/imagenes/cronograma.jpg' className='secciones__img' alt='cronograma'/>
                 <h3>Cronograma de actividades</h3>
             </div>
             <div className='secciones__item item3'>
-                <img src={lengua} width='300px' alt='biblioteca'/>
+                <StaticImage src='../../assets/imagenes/señas.jpg' className='secciones__img' alt='lenguaje'/>
                 <h3>Lengua de señas colombiana</h3>
             </div>
             <div className='secciones__item item4'>
-                <img src={horario} width='300px' alt='biblioteca'/>
+                <StaticImage src='../../assets/imagenes/horario.jpg' className='secciones__img' alt='horario'/>
                 <h3>Horario</h3>
             </div>
         </div>
